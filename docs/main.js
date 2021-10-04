c0f377e401e2d699c9a9d623b233300e54ea50b7
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-10-04 22:26:21 +0900

    cookieテスト

--- docs/main.js
+++ docs/main.js
@@ -24,10 +24,84 @@ $(function() {
     }
     $.get('aetheryte_id_list.csv', parseCsv, 'text');
 
+    // クッキーからデータ取得
     function cookie_get(str){
         $.each(id_suffix, function(_, value) {
             var key = str + value;
+            var cookie_data = $.cookie(key);
+            set_value(key, cookie_data);
+        });
+    }
+
+    // 取得したデータからinputに反映
+    function set_value(key, data){
+        if(data >= 1){
+            $(key).prop("checked", true);
+        } else {
+            $(key).prop("checked", false);
+        }
+    }
+
+
+    // クッキーに情報を格納する
+    $('input').change(function() {
+        if ($("#settingcookie").prop("checked") == true) {
+            cookie_all_set();
+        } else {
+            cookie_all_delete();
+        }
+    });
+
+    // クッキーに情報を格納
+    function cookie_all_set(){
+        $.get('aetheryte_id_list.csv', setparseCsv, 'text');
+    }
+    function setparseCsv(data) {
+        var csv = $.csv.toArrays(data);
+
+        //一行目は見出しなので削除
+        csv.shift();
+
+        $(csv).each(function(i) {
+            cookie_set(this[0].toString());
+        });
+    }
+    function cookie_set(str){
+        $.each(id_suffix, function(_, value) {
+            var key = str + value;
+            console.log(key);
+            if ($(key).prop("checked") == true) {
+                $.cookie(key, 1, { expires: 30 });
+                console.log(1);
+            } else {
+                $.cookie(key, -1, { expires: 30 });
+                console.log(-1);
+            }
+        });
+    }    
+
+    // クッキーに情報を格納しないチェックのときには、情報を全削除する
+    function cookie_all_delete(){
+        $.get('aetheryte_id_list.csv', deleteparseCsv, 'text');
+    }
+    function deleteparseCsv(data) {
+        var csv = $.csv.toArrays(data);
+
+        //一行目は見出しなので削除
+        csv.shift();
+
+        $(csv).each(function(i) {
+            cookie_delete(this[0].toString());
+        });
+    }
+    function cookie_delete(str){
+        $.each(id_suffix, function(_, value) {
+            var key = str + value;
+            $.removeCookie(key);
+            console.log("delete-----------");
             console.log(key);
         });
     }
+
+
 });

7324be7f6f5997fe7e702b19374d5d55aff69f89
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-10-04 21:22:41 +0900

    csv テスト

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,17 @@
-$(document).ready(function() {
-    
-
+$(function() {
+    function parseCsv(data) {
+        var csv = $.csv.toArrays(data);
+  
+        var insert = '';
+        $(csv).each(function(i) {
+            console.log("------");
+            if (this.length > 0) {
+                $(this).each(function() {
+                    console.log(this);
+                });
+            }
+        });
+    }
+    $.get('aetheryte_id_list.csv', parseCsv, 'text');
+  
 });

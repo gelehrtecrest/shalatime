098682192d53e0cbd7c87b2c7acc4f193311ca83
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-10-04 21:47:33 +0900

    id_suffixテスト

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,17 @@
+var id_suffix = [
+    '-start',
+    '-end',
+    '-pass',
+    '-free',
+    '-half',
+    '-homepoint',
+];
+
 $(function() {
 
-    
+    // クッキーから情報を取得する。
+    // CSVファイルからidを取得
+
     function parseCsv(data) {
         var csv = $.csv.toArrays(data);
 
@@ -8,10 +19,15 @@ $(function() {
         csv.shift();
 
         $(csv).each(function(i) {
-            console.log("------");
-            console.log(this[0].toString());
+            cookie_get(this[0].toString());
         });
     }
     $.get('aetheryte_id_list.csv', parseCsv, 'text');
-  
+
+    function cookie_get(str){
+        id_suffix.each(function(i) {
+            var key = str + this;
+            console.log(key);
+        });
+    }
 });

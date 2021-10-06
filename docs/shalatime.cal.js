cd589e4138dc909cea56392aa01db22645c1331b
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-10-06 23:46:00 +0900

    fix

--- docs/shalatime.cal.js
+++ docs/shalatime.cal.js
@@ -195,13 +195,12 @@ $(function() {
 
     // 接尾リストにあればreplaceで空白に書き換える
     function delete_suffix(str){
-        let output;
         console.log(str);
         id_suffix.forEach(function(suffix){
             console.log(suffix);
-            output = str.replace(suffix, '');
+            str = str.replace(suffix, '');
             console.log(output);
         });
-        return output;
+        return str;
     }
 });

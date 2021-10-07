43f82c0a7895de20f97462730136bf0df6b22321
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-10-07 20:33:31 +0900

    設定の保存を別ファイルに

--- docs/shalatime.setting.js
+++ docs/shalatime.setting.js
@@ -6,14 +6,20 @@ const id_suffix = [
     '-half',
     '-homepoint',
 ];
+
 const yes = "YES";
 const no = "NO";
+
+// ファイル名
+const filecsv_aetheryte = 'aetheryte_id_list.csv';
+const filecsv_setting = 'setting_id_list.csv';
 
 $(function() {
 
     // localStorageから情報を取得する。
     // CSVファイルからidを取得
 
+    // テレポ先の設定
     function parseCsv(data) {
         let csv = $.csv.toArrays(data);
 
@@ -24,7 +30,20 @@ $(function() {
             local_storage_get(this[1].toString());
         });
     }
-    $.get('aetheryte_id_list.csv', parseCsv, 'text');
+    $.get(filecsv_aetheryte, parseCsv, 'text');
+
+    // UIの設定
+    function parseSettingCsv(data) {
+        let csv = $.csv.toArrays(data);
+
+        //一行目は見出しなので削除
+        csv.shift();
+
+        $(csv).each(function(i) {
+            local_storage_setting_get(this[1].toString());
+        });
+    }
+    $.get(filecsv_setting, parseSettingCsv, 'text');
 
     // localStorageからデータ取得
     function local_storage_get(str){
@@ -34,6 +53,11 @@ $(function() {
             set_value(key, local_storage_data);
         });
     }
+    function local_storage_setting_get(key){
+        let local_storage_data = localStorage.getItem(key);
+        set_value(key, local_storage_data);
+    }
+
 
     // 取得したデータからinputに反映
     function set_value(key, local_storage_data){
@@ -61,7 +85,10 @@ $(function() {
 
     // localStorageに情報を格納
     function local_storage_all_set(){
-        $.get('aetheryte_id_list.csv', setparseCsv, 'text');
+        // テレポ先の設定
+        $.get(filecsv_aetheryte, setparseCsv, 'text');
+        // 設定
+        $.get(filecsv_setting, setparseSettingCsv, 'text');
     }
     function setparseCsv(data) {
         let csv = $.csv.toArrays(data);
@@ -73,8 +100,17 @@ $(function() {
             local_storage_set(this[1].toString());
         });
     }
+    function setparseSettingCsv(data) {
+        let csv = $.csv.toArrays(data);
+
+        //一行目は見出しなので削除
+        csv.shift();
+        set_data = new Object();
+        $(csv).each(function(i) {
+            local_storage_setting_set(this[1].toString());
+        });
+    }
     function local_storage_set(str){
-        
         $.each(id_suffix, function(_, value) {
             var key = str + value;
             if ($('#' + key).prop("checked") == true) {
@@ -82,7 +118,13 @@ $(function() {
             } else {
                 localStorage.setItem(key, no);
             }
-        });
-        
+        });    
+    }
+    function local_storage_setting_set(key){
+        if ($('#' + key).prop("checked") == true) {
+            localStorage.setItem(key, yes);
+        } else {
+            localStorage.setItem(key, no);
+        }
     }
 });

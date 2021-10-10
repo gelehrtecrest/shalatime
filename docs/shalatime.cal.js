2a12080f06c9bd10fd8bfc9fcd65c4e8920eb832
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-10-11 03:00:55 +0900

    fix

--- docs/shalatime.cal.js
+++ docs/shalatime.cal.js
@@ -547,7 +547,7 @@ $(function() {
                 str = get_aetheryte_name(point);
                 old_point = point;
             } else {
-                str = str + " →(" + calOrGetRouteCost([old_point, point]) + ")→" + get_aetheryte_name(point) + get_zero_or_half_name(point);
+                str = str + " →(" + calOrGetRouteCost([old_point, point]) + ")→ " + get_aetheryte_name(point) + get_zero_or_half_name(point);
             }
         });
 
